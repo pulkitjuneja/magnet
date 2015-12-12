4d02a07321038b046eda40b841cbd26945956802
b0rn2c0de <pulkit.16296@gmail.com>
2015-12-12 23:16:08 +0530

    library

---
+++ Assets/Magnet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magnet : MonoBehaviour {
+
+    public Rigidbody2D rigidbody;
+	void Start () 
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(0,-10f);
+	}
+	void Update () {
+	
+	}
+}
---
+++ Assets/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    string FieldObjectName = "magnetic field";
+    public GameObject Magnet; 
+	void Start () {
+	}
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == FieldObjectName)
+        {
+            Debug.Log(other.gameObject.transform.position);
+        }
+    }
+}
