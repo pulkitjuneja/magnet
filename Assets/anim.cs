4a47921875b3fb24ae58dc3d335a9239042ff69e
anshulsinghxd <anshulsinghxd@gmail.com>
2015-12-15 03:32:57 +0530

    animations

---
+++ Assets/anim.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anim : StateMachineBehaviour {
+
+	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		if (stateInfo.IsName ("absorb")) {
+			animator.SetInteger("state",0);
+		}
+	}
+}
