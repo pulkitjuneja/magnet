871c9f149c7616de8112bb2575113eb137a4ac3d
Pulkit Juneja <pulkit.16296@gmail.com>
2018-06-21 16:08:05 +0530

    updated spawner logic

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,41 +1,33 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
-    public bool CanSpawn = true;
+    public int collisionCount = 0;
     public GameObject ToSpawn = null;
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag != "level" && other.gameObject.tag!="absorbable")
-        {
-            CanSpawn = false;
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.tag != "level" && other.gameObject.tag != "absorbable") {
+            collisionCount++;
         }
     }
-    void OnTriggerExit2D(Collider2D other )
-    {
-        if (other.gameObject.tag != "level" && other.gameObject.tag != "absorbable")
-        {
-            CanSpawn = true;
-            if (ToSpawn != null)
-            {
-                Spawn(ToSpawn);
+    void OnTriggerExit2D (Collider2D other) {
+        if (other.gameObject.tag != "level" && other.gameObject.tag != "absorbable") {
+            collisionCount--;
+            if (collisionCount == 0 && ToSpawn != null) {
+                Spawn (ToSpawn);
                 ToSpawn = null;
             }
         }
     }
 
-    public void Spawn(GameObject spawn)
-    {
-        if (CanSpawn)
-        {
+    public void Spawn (GameObject spawn) {
+        Debug.Log (spawn);
+        if (collisionCount == 0) {
             var pos = this.transform.position;
-            GameObject pickup = Instantiate(spawn, pos, Quaternion.identity) as GameObject;
-        }
-        else
-        {
-            ToSpawn = spawn; 
+            GameObject pickup = Instantiate (spawn, pos, Quaternion.identity) as GameObject;
+        } else {
+            ToSpawn = spawn;
         }
     }
 }
